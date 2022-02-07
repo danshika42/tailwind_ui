e125de62b0baf2384ce96a8c34d0a1d74d64a966
danshika42 <83402199+danshika42@users.noreply.github.com>
2022-02-07 12:16:13 +0530

    --changed tailwind.config

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import Group from './Group.svg'
 
 function App() {
     return (
-    <div className="sm:bg-gradient-to-bl sm:from-[#161723] sm:via-[#131417] sm:to-[#131313] bg-gradient-to-bl from-[#1c2045] via-[#191c35] to-[#151620] h-full w-full">
+    <div className="sm:bg-gradient-radial-black bg-gradient-radial-blue h-full w-full">
      
       <div className="container m-auto w-full lg:w-9/12 h-full">
        
         <nav className='text-white font-bold p-12 flex justify-between items-center'>
-          <h2 className='text-2xl'>UNX</h2>
+          <h1 className='text-2xl'>UNX</h1>
           <ul className='sm:flex hidden justify-evenly items-center w-full md:w-7/12 '>
             <a href='#' >Products</a>
             <a href='#' >About</a>
@@ -24,9 +24,9 @@ function App() {
         <div className='text-center mt-10 mb-10'>
           <h1 className='text-white sm:text-6xl text-4xl  font-bold'>The Best Way to</h1>
 
-          <h1 className='sm:text-6xl text-4xl font-bold'>
-            <span className='text-transparent  bg-clip-text bg-gradient-to-r from-[#7399fb] to-[#a5d9ff]'>Track your Daily Progress</span>
-          </h1>
+          <div className='sm:text-6xl text-4xl font-bold'>
+            <span className='text-transparent  bg-clip-text bg-gradient-linear-blue'>Track your Daily Progress</span>
+          </div>
 
           <div className='m-auto w-full md:w-8/12'>
             <p className='m-20 text-md text-center text-white text-opacity-70 '>UNX Allows you to track all your daily task progress using gamification methods and earn rewards for specific coupons</p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,13 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage :{
+        "gradient-radial-blue":"radial-gradient(at left center ,#1c2045 , #191c35 , #151620)",
+        "gradient-radial-black":"radial-gradient(at left center ,#161723, #131417, #131313)",
+        "gradient-linear-blue":"linear-gradient(to right,#7399fb,#a5d9ff)",
+      }
+    },
   },
   plugins: [],
 }
